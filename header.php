ce9463edc6cb4ad440558d89dc3cc3fef015b5cf
Mathilde Arconte <mathilde@m-arconte.fr>
2017-01-15 02:03:47 +0100

    Style Header - WIP

--- header.php
+++ header.php
@@ -23,11 +23,11 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'medusa' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
+	<header id="masthead" class="site-header jumbotron" role="banner" style="background-image: url(<?php header_image(); ?>)">
+		<div class="site-branding container">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
 			<?php else : ?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php
@@ -44,7 +44,15 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'medusa' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
 
+
+<svg width="100%" height="auto" viewBox="0 -2 1440 82" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <!-- Generator: Sketch 41.2 (35397) - http://www.bohemiancoding.com/sketch -->
+    <desc>Created with Sketch.</desc>
+    <defs></defs>
+    <path d="M4.27318146,-3.19744231e-13 L0,-3.19744231e-13 L0,79.3653676 L1440,79.3653676 L1440,-3.19744231e-13 L1430.13036,-3.19744231e-13 C769.081287,169.131289 65.2881047,14.0606124 4.27318146,-1.00024433e-11 Z" id="header-bottom" stroke="none" fill="#FFFFFF" fill-rule="evenodd"></path>
+</svg>
+	</header>
+	<!-- #masthead -->
 	<div id="content" class="site-content">
 	<div class="container">
